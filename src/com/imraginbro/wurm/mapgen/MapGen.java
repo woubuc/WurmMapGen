dce5051abf9163abdb1e5609ebea8e7b28f44917
Wouter <wou.buc@gmail.com>
2017-12-06 09:25:11 +0100

    Output mapgen duration in milliseconds instead of seconds

--- src/com/imraginbro/wurm/mapgen/MapGen.java
+++ src/com/imraginbro/wurm/mapgen/MapGen.java
@@ -1,13 +1,13 @@
 package com.imraginbro.wurm.mapgen;
 
 public class MapGen {
-
+	
 	public static void main(String[] args) throws Exception {
 		final long startTime = System.currentTimeMillis();
 		new MapBuilder();
 		final long endTime = System.currentTimeMillis();
-		final long totalTime = (endTime - startTime)/1000;
-		System.out.println("Finished with map generation! " + totalTime + " seconds.");
+		final long totalTime = (endTime - startTime);
+		System.out.println("Map generated in " + totalTime + " milliseconds.");
 	}
 	
 }
