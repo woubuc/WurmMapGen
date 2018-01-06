2fd85b2f20d337edc338c0d3cac164f707650b7b
Wouter <wou.buc@gmail.com>
2018-01-06 10:02:28 +0100

    Added more logging to database handler for debugging purposes

--- src/be/woubuc/wurmunlimited/wurmmapgen/database/DatabaseHandler.java
+++ src/be/woubuc/wurmunlimited/wurmmapgen/database/DatabaseHandler.java
@@ -99,6 +99,8 @@ public class DatabaseHandler {
 	 * @return  The modded IDs list
 	 */
 	private ArrayList<Integer> loadModdedTemplateIds(String[] templateNames) {
+		Logger.details("Loading modded template IDs from modsupport.db");
+		
 		ArrayList<Integer> ids = new ArrayList<>();
 		if (modSupport == null) {
 			Logger.warn("Mod support database not loaded");
@@ -106,19 +108,24 @@ public class DatabaseHandler {
 		}
 		
 		StringBuilder query = new StringBuilder();
-		query.append("select `ID` from `IDS` where ");
+		query.append("select `ID`, `NAME` from `IDS` where ");
 		for (int i = 1; i < templateNames.length; i++) query.append("`NAME` = ? or ");
 		query.append("`NAME` = ?;");
 		
 		try (PreparedStatement statement = modSupport.prepareStatement(query.toString(), (Object[]) templateNames);
 			 ResultSet resultSet = statement.executeQuery()) {
 			
-			while (resultSet.next()) ids.add(resultSet.getInt("ID"));
+			while (resultSet.next()){
+				final int id = resultSet.getInt("ID");
+				ids.add(id);
+				
+				Logger.details("-> " + resultSet.getString("NAME") + " (" + id + ")");
+			}
 		} catch (SQLException e) {
 			Logger.error("Could not load modded items: " + e.getMessage());
 		}
 		
-		Logger.ok("Loaded " + ids.size() + " IDs from modsupport.db", true);
+		Logger.ok("Loaded " + ids.size() + " mod templates", true);
 		return ids;
 	}
 	
@@ -127,11 +134,13 @@ public class DatabaseHandler {
 	 * @return  The guard towers
 	 */
 	public ArrayList<GuardTower> getGuardTowers() {
-		Logger.details("Loading guard towers from wurmitems.db");
 		
 		// Get modded tower IDs
 		List<Integer> towerIds = new ArrayList<>(Arrays.asList(DatabaseHandler.towerIds));
 		towerIds.addAll(loadModdedTemplateIds(towerTemplateNames));
+		
+		Logger.details("Loading guard tower items from wurmitems.db");
+		Logger.details("-> Tower IDs: " + towerIds.toString());
 		
 		// Build query based on the number of tower IDs
 		StringBuilder query = new StringBuilder();
@@ -165,7 +174,8 @@ public class DatabaseHandler {
 	 * @return  The portals
 	 */
 	public ArrayList<Portal> getPortals() {
-		Logger.details("Loading portals from wurmitems.db");
+		Logger.details("Loading portal items from wurmitems.db");
+		Logger.details("-> Portal IDs: " + Arrays.toString(portalIds));
 		
 		// Build query based on the number of portal IDs
 		StringBuilder query = new StringBuilder();
